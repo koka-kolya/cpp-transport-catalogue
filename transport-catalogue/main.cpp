c9b45a8d08a0a5e955e8ddd13a49b19953b0129b
koka-kolya <112828943+koka-kolya@users.noreply.github.com>
2023-04-06 17:17:32 +0300

    little thing

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -6,11 +6,9 @@
 #include <string_view>
 #include <fstream>
 
-using namespace std::literals;
-
 int main() {
 	data_base::TransportCatalogue data_base; // create empty data base
-	router::TransportRouter transport_router; // creat transport router
+	router::TransportRouter transport_router; // create transport router
 	json_reader::JsonReader json_reader(data_base, transport_router); // initialize json_reader
 	json_reader.LoadJsonAndSetDB(std::cin);	// load JSON requests and contain data base
 	json_reader.GetCompleteOutputJSON(std::cout); // get JSON info from data base to ostream
